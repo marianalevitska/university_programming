6a33d11a1dc32645589879bc379dac18dfcd1d96
Mariana <mariamna5204@gmail.com>
2022-04-11 21:27:30 +0300

    lab2

--- lab_3_2.cpp
+++ lab_3_2.cpp
@@ -1,22 +1,44 @@
-// #include <iostream>
-// #include <iomanip>
-// #include <cmath>
-// using namespace std;
-// int main()
-// {
-//     setlocale(0, "");
-//     int a;
-//     cout << "Enter data:\n"
-//          << endl;
-//     cout << "a=";
-//     cin >> a;
-//     switch (expression)
-//     {
-//     case /* constant-expression */:
-//         /* code */
-//         break;
-
-//     default:
-//         break;
-//     }
-// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    int ch;
+    cout << "\nEnter the week day number ";
+    cin >> ch;
+    switch (ch)
+    {
+    case 1:
+        cout << "\n"
+             << ch << " - Monday" << endl;
+        break;
+    case 2:
+        cout << "\n"
+             << ch << " - Tuesday" << endl;
+        break;
+    case 3:
+        cout << "\n"
+             << ch << "- Wednesday" << endl;
+        break;
+    case 4:
+        cout << "\n"
+             << ch << " - Thursday" << endl;
+        break;
+    case 5:
+        cout << "\n"
+             << ch << " - Friday" << endl;
+        break;
+    case 6:
+        cout << "\n"
+             << ch << "- Saturday" << endl;
+        break;
+    case 7:
+        cout << "\n"
+             << ch << "- Sunday" << endl;
+        break;
+    default:
+    {
+        cout << "\n MISTAKE!!!" << endl;
+        break;
+    }
+    }
