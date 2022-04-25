74e22ef7bedcd6a56790e5fce705cbd3b40199f2
Mariana <mariamna5204@gmail.com>
2022-04-26 00:00:20 +0300

    //

--- lab_4/lab_4.cpp
+++ lab_4/lab_4.cpp
@@ -8,14 +8,15 @@ using namespace std;
 
 double f(double x)
 {
-  return (pow(x, 2.0) + x);
+  return ((2 * pow(x, 4.0) + x * x + 2 * x + 1) / (x * x + 1));
 }
 int main()
 {
-  double a = 2.0, b = 3.0;
-  int n = 50;
+  double a, b;
+  int n;
+  cin >> a >> b >> n;
   // double s = (f(a) + f(b)) / 2;
-  double s = (f(b) - f(a)) / 2;
+  double s = (f(b) + f(a)) / 2;
   double h = (b - a) / n;
   for (int i = 0; i <= n - 1; i++)
   {
