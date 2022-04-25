b1acd942ee1c76c3d33d0e7972f3665c9ff2247f
Mariana <mariamna5204@gmail.com>
2022-04-26 00:23:02 +0300

    //

--- lab_4/lab_4.cpp
+++ lab_4/lab_4.cpp
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <math.h>
 using namespace std;
-
 double f(double x)
 {
   return ((2 * pow(x, 4.0) + x * x + 2 * x + 1) / (x * x + 1));
@@ -21,6 +20,26 @@ int main()
   }
   double I = (h * s) / 2;
   cout << setprecision(10) << I << endl;
-
-  return 0;
+  cout << "Do you want to check again ?" << endl;
+  cout << "Write 1 if yes or 0 if no" << endl;
+  int c;
+  cin >> c;
+  if (c == 1)
+  {
+    double a, b;
+    int n;
+    cin >> a >> b >> n;
+    double s = (f(b) + f(a)) / 2;
+    double h = (b - a) / n;
+    for (int i = 0; i <= n - 1; i++)
+    {
+      s += f(a + i * h);
+    }
+    double I = (h * s) / 2;
+    cout << setprecision(10) << I << endl;
+  }
+  else
+  {
+    return 0;
+  }
 }
