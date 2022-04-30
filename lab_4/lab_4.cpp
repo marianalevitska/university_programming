7d0848f5c61694a81e2e2b6240bf915d263197c6
Mariana <mariamna5204@gmail.com>
2022-04-30 15:23:28 +0300

    //

--- lab_4/lab_4.cpp
+++ lab_4/lab_4.cpp
@@ -1,3 +1,49 @@
+// #include <iostream>
+// #include <iomanip>
+// #include <cmath>
+// #include <math.h>
+// using namespace std;
+// double f(double x)
+// {
+//   return ((2 * pow(x, 4.0) + x * x + 2 * x + 1) / (x * x + 1));
+// }
+// int main()
+// {
+//   double a, b;
+//   int n;
+//   cin >> a >> b >> n;
+//   double s = (f(b) + f(a)) / 2;
+//   double h = (b - a) / n;
+//   for (int i = 0; i <= n - 1; i++)
+//   {
+//     s += f(a + i * h);
+//   }
+//   double I = (h * s) / 2;
+//   cout << setprecision(10) << I << endl;
+//   cout << "Do you want to check again ?" << endl;
+//   cout << "Write 1 if yes or 0 if no" << endl;
+//   int c;
+//   cin >> c;
+//   if (c == 1)
+//   {
+//     double a, b;
+//     int n;
+//     cin >> a >> b >> n;
+//     double s = (f(b) + f(a)) / 2;
+//     double h = (b - a) / n;
+//     for (int i = 0; i <= n - 1; i++)
+//     {
+//       s += f(a + i * h);
+//     }
+//     double I = (h * s) / 2;
+//     cout << setprecision(4) << I << endl;
+//   }
+//   else
+//   {
+//     return 0;
+//   }
+// }
+
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -12,34 +58,13 @@ int main()
   double a, b;
   int n;
   cin >> a >> b >> n;
-  double s = (f(b) + f(a)) / 2;
+
+  double Left(double a, double b, int n);
   double h = (b - a) / n;
-  for (int i = 0; i <= n - 1; i++)
+  double Intgrl = 0.0;
+  for (int i = 0; i <= n; i++)
   {
-    s += f(a + i * h);
+    Intgrl += f(a + i * h);
   }
-  double I = (h * s) / 2;
-  cout << setprecision(10) << I << endl;
-  cout << "Do you want to check again ?" << endl;
-  cout << "Write 1 if yes or 0 if no" << endl;
-  int c;
-  cin >> c;
-  if (c == 1)
-  {
-    double a, b;
-    int n;
-    cin >> a >> b >> n;
-    double s = (f(b) + f(a)) / 2;
-    double h = (b - a) / n;
-    for (int i = 0; i <= n - 1; i++)
-    {
-      s += f(a + i * h);
-    }
-    double I = (h * s) / 2;
-    cout << setprecision(4) << I << endl;
-  }
-  else
-  {
-    return 0;
-  }
+  return Intgrl;
 }
