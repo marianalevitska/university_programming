2953bf575126cbb0acbc0f848b5b98f6ed2f0d4e
Mariana <mariamna5204@gmail.com>
2022-05-01 14:09:57 +0300

    lab-5

--- lab_5/lab_5.cpp
+++ lab_5/lab_5.cpp
@@ -9,21 +9,23 @@ int main()
     setlocale(0, "");
     int k;
     3 <= k <= 10;
-    cout << "\n\t\t generating random numbers \n\n\n";
+    cout << "generating random numbers" << endl;
     srand(time(NULL)); // ініціалізація початкового заповнення
-    cout << "\n\n\trandom integers in the interval [-25;30]:\n";
+    cout << "trandom integers in the interval [-25;30]: ";
     for (int i = 0; i < k; i++)
     {
         if (i % 10 == 0)
             cout << "\n"; // генерація цілого випадкового числа
-        int number = -25 + rand() % 30;
+        int numb = -25 + rand() % 30;
+        cout << numb << "\t";
     }
+
     cout << "\n\n\tvalid random numbers in the interval [1;5]:\n";
     for (int i = 0; i < k; i++)
     {
         if (i % 10 == 0)
             cout << "\n"; // генерація дійсного випадкового числа
-        float number = 1 + rand() % 5;
+        float number = 1.0 + (float)rand() / RAND_MAX * 10 - 5.0;
         cout << setprecision(2) << number << "\t";
     }
     system("pause");
