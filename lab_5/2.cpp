26a660b46762c978cb489bc404859bae69a1d115
Mariana <mariamna5204@gmail.com>
2022-05-08 14:51:47 +0300

    product

--- lab_5/2.cpp
+++ lab_5/2.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
-
+#include <windows.h>
+#include <conio.h>
 using namespace std;
 
 int main()
 {
     setlocale(0, "");
-    int n, a, b, k;
+    int n, a, b, k, nmin, nmax, min, max, tmp;
     int line[n];
     int i;
+    int d;
+    d = 1;
     k = 0;
     cout << "\n\n\tEnter the beginnig of numbers line (a): ";
     cin >> a;
@@ -33,5 +36,44 @@ int main()
             k += line[i];
         }
     }
+
     cout << "\n\n\tthe sum of negative values on the segment [" << a << ";" << b << "] is " << k << endl;
+    getch();
+    max = line[1];
+    min = line[1];
+
+    for (i = 2; i <= n; i++)
+    {
+        if (line[i] > max)
+        {
+            max = line[i];
+            nmax = i;
+        }
+        if (line[i] < min)
+        {
+            min = line[i];
+            nmin = i;
+        }
+    }
+
+    if (nmin > nmax)
+    {
+        for (i = nmax; i <= nmin; i++)
+        {
+            d = d * line[i];
+        }
+    }
+    else
+    {
+        for (i = nmin; i <= nmax; i++)
+        {
+            d = d * line[i];
+        }
+    }
+
+    cout << "\n\n\tMinimal value [" << nmin << "]=" << min << "\n";
+    cout << "\n\n\tMaximal value [" << nmax << "]=" << max << "\n";
+    cout << "\n\n\tProduct of values between min and max = " << d << endl;
+
+    getch();
 }
