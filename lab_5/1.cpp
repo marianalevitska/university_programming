4ea4035955dab77ace048b6e2739e611dcef8856
Mariana <mariamna5204@gmail.com>
2022-05-10 17:26:06 +0300

    //

--- lab_5/1.cpp
+++ lab_5/1.cpp
@@ -7,27 +7,44 @@ using namespace std;
 int main()
 {
     setlocale(0, "");
-    int k;
+    int k, m, n;
     cout << "\n\n\tenter the number of values in one line( 3 <= k <= 10):\n ";
     cin >> k;
+    cout << "\n\n\tenter the number of integers:\n ";
+    cin >> m;
+    cout << "\n\n\tenter the number of valids:\n ";
+    cin >> n;
     cout << "\n\n\tGENERATING RANDOM NUMBERS\n";
     srand(time(0)); // ініціалізація початкового заповнення
     cout << "\n\n\trandom integers in the interval [-25;30]: \n";
-    for (int i = 0; i < k; i++)
+    for (int i = 0; i < m; i++)
     {
-        if (i % 10 == 0)
+        if (i % k == 0 && m % k == 0)
+        {
             cout << "\n"; // генерація цілого випадкового числа
-        int numb = -25 + rand() % 30;
-        cout << numb << "\t";
+            int numb = -25 + rand() % 30;
+            cout << numb << "\t";
+        }
+        else
+        {
+
+            cout << "The number of integers is wrong" << endl;
+        }
     }
 
     cout << "\n\n\tvalid random numbers in the interval [1;5]:\n";
-    for (int i = 0; i < k; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (i % 10 == 0)
+        if (i % k == 0 && n % k == 0)
+        {
             cout << "\n"; // генерація дійсного випадкового числа
-        float number = 1.0 + (float)rand() / RAND_MAX * 10 - 5.0;
-        cout << setprecision(2) << number << "\t";
+            float number = 1.0 + (float)rand() / RAND_MAX * 10 - 5.0;
+            cout << setprecision(2) << number << "\t";
+        }
+        else
+        {
+            cout << "The number of valids is wrong" << endl;
+        }
     }
     system("pause");
     return 0;
