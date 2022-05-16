b1e3a6e8bbd58069418ae3a7368d96b94fcc0a72
Mariana <mariamna5204@gmail.com>
2022-05-17 00:54:10 +0300

    //

--- lab_7/2.cpp
+++ lab_7/2.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool check_polindrom(string str)
 {
 
-    for (int i = 0; i < str.length(); i++)
+    for (int i = 0; i < str.length() / 2; i++)
     {
         if (str[i] != str[str.length() - i - 1])
         {
