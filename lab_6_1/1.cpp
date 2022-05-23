6fe3b1a6fd497f53e0f2bd10bb3bf66c839c258d
Mariana <mariamna5204@gmail.com>
2022-05-23 13:29:17 +0300

    //

--- lab_6_1/1.cpp
+++ lab_6_1/1.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 int main()
@@ -9,7 +14,7 @@ int main()
     int N, M, i, j, a, b;
     int n = N + 1;
     int m = M + 1;
-    int S = 0, S1 = 0, S2 = 0;
+    // int S = 0, S1 = 0, S2 = 0;
     int line[n][m];
     cout << "\nEnter maximal value of lines N: ";
     cin >> N;
@@ -45,18 +50,50 @@ int main()
             cout << setw(5) << line[i][j];
         }
     }
-    for (int i = 1; i < n; i++)
+    // do
+    // {
+    //     for (int i = 1; i < n; i++)
+    //     {
+    //         S1 = 0;
+    //         for (int j = 1; j < m; j++)
+    //         {
+    //             S += line[i][j];
+    //             if (S > S1)
+    //             {
+    //                 S1 = S;
+    //                 S2 = i + 1;
+    //             }
+    //         }
+    //     }
+    // } while (S > S1);
+    //  cout << "\nThe biggest sum value is in line: " << S2;
+    int str_sum[] = {};
+    int max_str;
+    int max_str_id;
+    for (i = 0; i < n; i++)
     {
-
-        for (int j = 1; j < m; j++)
+        str_sum[i] = 0;
+        for (j = 0; j < m; j++)
         {
-            S += line[i][j];
+            printf("[%d][%d]=%d ", i, j, line[i][j]);
+            str_sum[i] += line[i][j]; //вычисляем сумму эл-тов строки
         }
-        if (S > S1)
+        printf("\nSum of line's elements: %d \n", str_sum[i]);
+    }
+    int x;
+    max_str = str_sum[0];
+    for (x = 0; x < m; x++)
+    {
+        if (max_str < str_sum[x])
         {
-            S1 = S;
-            S2 = i + 1;
+            max_str = str_sum[x];
+            max_str_id = x;
         }
     }
-    cout << "\nThe biggest sum value is in line: " << S2;
+    printf("Max sum = %d and line number - %d  \n \n", max_str, max_str_id);
+
+    for (i = 0; i < m; i++)
+    {
+        printf("[%d][%d]=%d ", max_str_id, i, line[max_str_id][i]);
+    }
 }
