63031bf658efa96d1c5f4aed16c2c9c02c61b2bb
Mariana <mariamna5204@gmail.com>
2022-05-28 00:18:10 +0300

    1 done

--- lab_9/1.cpp
+++ lab_9/1.cpp
@@ -7,38 +7,52 @@
 // якщо таких немає, то вивести повідомлення.
 #include <iostream>
 #include <string>
+#define m 5
+#define n 3
 using namespace std;
-struct STUD //описуємо структуру STUD
+struct stud //описуємо структуру STUD
 {
-    string NAME;
-    int GROUP;
-    int SES[5] = {};
+    string name;
+    string group;
+    int ses[m];
 };
-void first(struct STUD)
-{
-    STUD student;
-    cout
-        << "Enter name: ";
-    cin >> student.NAME;
-    cout << "Enter group: ";
-    cin >> student.GROUP;
-    cout << "Enter 5 marks: ";
-    for (int i = 0; i < 5; i++)
-    {
-        cin >> student.SES[i];
-    }
-    cout << "Name: " << student.NAME << endl;
-    cout << "Group: " << student.GROUP << endl;
-    cout << "Marks: ";
-    for (int i = 0; i < 5; i++)
-    {
-        cout << student.SES[i] << " ";
-    }
-    cout << endl;
-}
+
 int main()
 {
-    STUD student;
-    first(student);
+    int count = 0;
+    int sum = 0;
+    stud student[n];
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Please enter student's NAME: ";
+        cin >> student[i].name;
+        cout << "Please enter student's GROUP: ";
+        cin >> student[i].group;
+        cout << "Please enter student's GRADES: " << endl;
+        for (j = 0; j < m; j++)
+        {
+            cin >> student[i].ses[j];
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            sum += student[i].ses[j];
+        if (sum / 5 >= 4)
+        {
+            cout << "student which point above 4 " << endl;
+            cout << "Name " << student[i].name << " "
+                 << "Group " << student[i].group << endl;
+            count++;
+        }
+
+        sum = 0;
+    }
+
+    if (count == 0)
+        cout << "There are no students with average grade more than 4.0" << endl;
+
     return 0;
 }
