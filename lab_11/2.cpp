285fdf63a396081158f46f56c630885d645232f6
Mariana <mariamna5204@gmail.com>
2022-05-31 14:42:24 +0300

    **

--- lab_11/2.cpp
+++ lab_11/2.cpp
@@ -10,43 +10,81 @@ struct stud //описуємо структуру STUD
     string group;
     int ses[m];
 };
-void FillArray(stud *student);
+void FillArray(stud *student)
 {
     string text = "";
     ofstream createFile("theList.txt");
     createFile.close();
-    ifstream readFile("theList.txt");
-    readFile.close();
+    // ifstream readFile("theList.txt");
+    // readFile.close();
+    fstream myFile;
+    // myFile.clear();
+    myFile.open("theList.txt", ios::out);
+
     for (int i = 0; i < n; i++)
     {
-        cout << "Please enter student's NAME: ";
+        cout << "Enter name: ";
+        myFile << "Please enter student's NAME: ";
         cin >> student[i].name;
-        cout << "Please enter student's GROUP: ";
+        myFile << student[i].name;
+        myFile << endl;
+        cout << "Enter group: ";
+        myFile << "Please enter student's GROUP: ";
         cin >> student[i].group;
-        cout << "Please enter student's GRADES: " << endl;
+        myFile << student[i].group;
+        myFile << endl;
+        cout << "Enter marks: ";
+        myFile << "Please enter student's GRADES: " << endl;
         for (int j = 0; j < m; j++)
         {
             cin >> student[i].ses[j];
+            myFile << student[i].ses[j];
+            myFile << endl;
         }
+        myFile << endl;
     }
-    ofstream writeFile("theList.txt");
-    writeFile << text;
-    writeFile.close();
+    myFile.close();
+    // ofstream writeFile("theList.txt");
+    // writeFile << text;
+    // writeFile.close();
+}
+void Print()
+{
+    fstream myFile;
+    myFile.open("theList.txt", ios::in);
+    if (myFile.is_open())
+    {
+        string line;
+        while (getline(myFile, line))
+        {
+            cout << line << endl;
+        }
+        myFile.close();
+    }
 }
 void AddInfo()
 {
+    fstream myFile;
+    // myFile.clear();
+    myFile.open("theList.txt", ios::app);
     cout << endl;
     cout << "Add some information: " << endl;
     string add;
-    getline(cin, add);
-    ofstream addFile("theList.txt", ios_base::app);
-    addFile << add << endl;
-    addFile.close();
+    cin >> add;
+    myFile << add;
+    myFile.close();
     cout << endl;
     Print();
 }
 void findStudent(stud *student, int sum, int count)
 {
+    ofstream createFile("theList.txt");
+    createFile.close();
+    ifstream readFile("theList.txt");
+    readFile.close();
+    fstream myFile;
+    // myFile.clear();
+    myFile.open("theList.txt", ios::app);
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -54,8 +92,11 @@ void findStudent(stud *student, int sum, int count)
         if (sum / 5 >= 4)
         {
             cout << "student which point above 4 " << endl;
+            myFile << "student which point above 4 " << endl;
             cout << "Name " << student[i].name << " "
                  << "Group " << student[i].group << endl;
+            myFile << "Name " << student[i].name << " "
+                   << "Group " << student[i].group << endl;
             count++;
         }
 
@@ -64,16 +105,8 @@ void findStudent(stud *student, int sum, int count)
 
     if (count == 0)
         cout << "There are no students with average grade more than 4.0" << endl;
-}
-void Print()
-{
-    string text;
-    ifstream readFile("theList.txt");
-    while (getline(readFile, text))
-    {
-        cout << text << endl;
-    }
-    readFile.close();
+    myFile << "There are no students with average grade more than 4.0" << endl;
+    myFile.close();
 }
 
 int main()
@@ -82,42 +115,13 @@ int main()
     int sum = 0;
     stud student[n];
     int i, j;
-    stud *SIGN = new stud[n];
-    SIGN->FillArray(SIGN);
-    SIGN->Print();
-    SIGN->findStudent(SIGN);
-    SIGN->AddInfo();
+    fstream myFile;
+    // myFile.clear();
+    FillArray(student);
+    cout << "Print the text file info: " << endl;
+    Print();
+    findStudent(student, sum, count);
+    AddInfo();
 
-    // for (i = 0; i < n; i++)
-    // {
-    //     cout << "Please enter student's NAME: ";
-    //     cin >> student[i].name;
-    //     cout << "Please enter student's GROUP: ";
-    //     cin >> student[i].group;
-    //     cout << "Please enter student's GRADES: " << endl;
-    //     for (j = 0; j < m; j++)
-    //     {
-    //         cin >> student[i].ses[j];
-    //     }
-    // }
-
-    // for (i = 0; i < n; i++)
-    // {
-    //     for (j = 0; j < m; j++)
-    //         sum += student[i].ses[j];
-    //     if (sum / 5 >= 4)
-    //     {
-    //         cout << "student which point above 4 " << endl;
-    //         cout << "Name " << student[i].name << " "
-    //              << "Group " << student[i].group << endl;
-    //         count++;
-    //     }
-
-    //     sum = 0;
-    // }
-
-    // if (count == 0)
-    //     cout << "There are no students with average grade more than 4.0" << endl;
-
-    // return 0;
+    return 0;
 }
